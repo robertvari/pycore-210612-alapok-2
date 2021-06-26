843c4919fa383125f3dfef3c25b91d5af17886d3
Robert Vári <mail.robertvari@gmail.com>
2021-06-26 14:58:47 +0200

    update

--- 02_parameters.py
+++ 02_parameters.py
@@ -8,3 +8,19 @@ def user_data(name: str, age: int, email: str):
     print(f"Name: {name}")
     print(f"Age: {age}")
     print(f"Email: {email}")
+
+
+# default parameters
+def user_data2(name, age=10, email="peter@gmail.com"):
+    print(f"Name: {name}")
+    print(f"Age: {age}")
+    print(f"Email: {email}")
+
+user_data2("Robert", 42, "robert@gmail.com")
+
+# keyword arguments
+user_data2(
+    email="robert@gmail.com",
+    name="Robert",
+    age=42
+)
