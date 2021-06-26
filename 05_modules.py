1028fa1ea45bc7144dc413b5b9d35afea9cf473a
Robert Vári <mail.robertvari@gmail.com>
2021-06-26 17:08:11 +0200

    update

--- 05_modules.py
+++ 05_modules.py
@@ -1,4 +1,4 @@
 from utilities import file_utils
 
-# root_folder = r"C:\Work\_PythonSuli\pycore-210612\photos"
-# photos = file_utils.get_photos(root_folder)
+root_folder = r"C:\Work\_PythonSuli\pycore-210612\photos"
+photos = file_utils.get_files(root_folder)
--- utilities/file_utils.py
+++ utilities/file_utils.py
@@ -1,7 +1,7 @@
 import os
 
 
-def get_photos(root_folder: str) -> list:
+def get_files(root_folder: str, ext=".jpg") -> list:
     """
     Lists a folder and returns .jpg images
     :param root_folder: str
@@ -9,13 +9,12 @@ def get_photos(root_folder: str) -> list:
     """
     assert os.path.exists(root_folder), "Folder does not exist."
 
-    files = [i for i in os.listdir(root_folder) if i.lower().endswith(".jpg")]
+    files = [i for i in os.listdir(root_folder) if os.path.splitext(i)[1].lower() == ext.lower()]
 
     return files
 
 
-
 if __name__ == '__main__':
     folder_path = r"C:\Work\_PythonSuli\pycore-210612\photos"
-    result = get_photos(folder_path)
+    result = get_files(folder_path, ext=".XLSX")
     print(result)
