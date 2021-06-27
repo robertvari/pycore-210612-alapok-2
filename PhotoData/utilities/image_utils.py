5ece08f40c2fffd30cbfb2b5c69ff8328d83f8ee
Robert Vári <mail.robertvari@gmail.com>
2021-06-27 17:37:53 +0200

    update

--- PhotoData/utilities/image_utils.py
+++ PhotoData/utilities/image_utils.py
@@ -1,17 +1,33 @@
-import threading, time, queue
+import threading, time, queue, os
+from PIL import Image
 
 job_list = queue.Queue()
+thumb_size = 128
 
 
 def process_image_worker():
     while not job_list.empty():
-        job = job_list.get()
-        print(job)
-        time.sleep(1)
+        image_path = job_list.get()
+        print(f"Resizing image: {image_path}")
+
+        image_folder = os.path.dirname(image_path)
+        thumbnail_dir = os.path.join(image_folder, "_thumbnails")
+        if not os.path.exists(thumbnail_dir):
+            os.makedirs(thumbnail_dir)
+
+        # open image
+        img = Image.open(image_path)
+
+        # resize image
+        img.thmbnail(thumb_size, thumb_size)
+
+        # save thumbnail
+        img.save(os.path.join(thumbnail_dir, os.path.basename(image_path)))
+
         job_list.task_done()
 
 
-def start_processing(image_list, max_threads=4):
+def start_processing(image_list, max_threads=1):
     [
        job_list.put(i) for i in image_list
     ]
