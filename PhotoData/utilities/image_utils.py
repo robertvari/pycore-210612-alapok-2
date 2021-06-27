aea009207df2888414ba15119e4d88fb2097316f
Robert Vári <mail.robertvari@gmail.com>
2021-06-27 17:49:49 +0200

    update

--- PhotoData/utilities/image_utils.py
+++ PhotoData/utilities/image_utils.py
@@ -1,8 +1,9 @@
 import threading, time, queue, os
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
 
 job_list = queue.Queue()
-thumb_size = 128
+thumb_size = 300
+watermark_text = "Python Suli"
 
 
 def process_image_worker():
@@ -21,13 +22,18 @@ def process_image_worker():
         # resize image
         img.thumbnail((thumb_size, thumb_size))
 
+        # draw on image
+        font = ImageFont.truetype("arial.ttf", 100)
+        draw = ImageDraw.Draw(img)
+        draw.text((0, 0), watermark_text, fill=(255, 0, 0), font=font)
+
         # save thumbnail
         img.save(os.path.join(thumbnail_dir, os.path.basename(image_path)))
 
         job_list.task_done()
 
 
-def start_processing(image_list, max_threads=1):
+def start_processing(image_list, max_threads=6):
     [
        job_list.put(i) for i in image_list
     ]
