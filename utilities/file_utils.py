b36421ae06aef785f357bdf856d842191fefa4b9
Robert Vári <mail.robertvari@gmail.com>
2021-06-27 14:37:24 +0200

    update

--- utilities/file_utils.py
+++ utilities/file_utils.py
@@ -1,21 +1,28 @@
 import os
 
 
-def get_files(root_folder: str, ext=".jpg") -> list:
+def get_files(root_folder: str, ext=None) -> list:
     """
     Lists a folder and returns files.
     :param root_folder: str
     :param ext: optional extension filter
     :return: string list of file paths
     """
-    assert os.path.exists(root_folder), "Folder does not exist."
 
-    files = [
-        os.path.join(root_folder, i) for i in os.listdir(root_folder)
-        if os.path.splitext(i)[1].lower() == ext.lower()
+    # collect all content (files/folders)
+    folder_content = os.listdir(root_folder)
+
+    all_files = [
+        os.path.join(root_folder, i) for i in folder_content
+        if os.path.isfile(os.path.join(root_folder, i))
     ]
 
-    return files
+    if ext:
+        pass
+
+    assert os.path.exists(root_folder), "Folder does not exist."
+
+    return []
 
 
 if __name__ == '__main__':
