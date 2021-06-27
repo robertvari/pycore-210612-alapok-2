25d365e24f16de4fcd3010dd0200c78cbf1cf923
Robert Vári <mail.robertvari@gmail.com>
2021-06-27 15:58:31 +0200

    update

---
+++ 10_threading.py
@@ -0,0 +1,24 @@
+import time, random, threading
+
+
+file_list = [....]
+
+def worker1(worker_name):
+    print(f"{worker_name} started")
+    time.sleep(random.randint(10, 30))
+    print(f"{worker_name} finished")
+
+
+def worker2(worker_name):
+    print(f"{worker_name}  started")
+    time.sleep(random.randint(10, 30))
+    print(f"{worker_name}  finished")
+
+
+t1 = threading.Thread(target=worker1, args=["Worker1"])
+t2 = threading.Thread(target=worker2, args=["Worker2"])
+
+t1.start()
+t2.start()
+
+print("Program finished.")
