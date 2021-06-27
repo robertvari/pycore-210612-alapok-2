17bb09779e927d0808ff67db5584ec96c9e186cc
Robert Vári <mail.robertvari@gmail.com>
2021-06-27 14:45:58 +0200

    update

--- utilities/file_utils.py
+++ utilities/file_utils.py
@@ -12,13 +12,18 @@ def get_files(root_folder: str, ext=None) -> list:
     # collect all content (files/folders)
     folder_content = os.listdir(root_folder)
 
+    # collect all filer
     all_files = [
         os.path.join(root_folder, i) for i in folder_content
         if os.path.isfile(os.path.join(root_folder, i))
     ]
 
+    # if we are using extension filter
     if ext:
-        pass
+        all_files = [
+            i for i in all_files
+            if os.path.splitext(i)[1].lower() == ext.lower()
+        ]
 
     assert os.path.exists(root_folder), "Folder does not exist."
 
@@ -26,6 +31,6 @@ def get_files(root_folder: str, ext=None) -> list:
 
 
 if __name__ == '__main__':
-    folder_path = r"C:\Work\_PythonSuli\pycore-210612\photos"
-    result = get_files(folder_path, ext=".XLSX")
-    print(result)
+    #folder_path = r"C:\Work\_PythonSuli\pycore-210612\photos"
+    # result = get_files(folder_path)
+    print(os.path.splitext(r"C:\Work\_PythonSuli\pycore-210612\photos\IMG_1069.JPG"))
