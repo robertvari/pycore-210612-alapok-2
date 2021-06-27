7dbcd8c526866dcb0d7e2820f4f70649158a0151
Robert Vári <mail.robertvari@gmail.com>
2021-06-27 16:28:55 +0200

    update

--- 11_queue.py
+++ 11_queue.py
@@ -1,10 +1,18 @@
 import time, random, threading, queue
 from utilities.file_utils import get_files
 
-file_list = get_files(r"C:\Work\_PythonSuli\pycore-210612\photos", ext=".jpg")
+job_list = queue.Queue()
+[
+    job_list.put(i) for i in get_files(r"C:\Work\_PythonSuli\pycore-210612\photos", ext=".jpg")
+]
 
 
 def worker():
-    print("worker started")
-    time.sleep(random.randint(1, 10))
-    print("worker finished")
+    while not job_list.empty():
+        next_file = job_list.get()
+
+        print(f"Working on: {next_file}")
+        time.sleep(random.randint(1, 10))
+        print("Worker finished")
+
+        job_list.task_done()
